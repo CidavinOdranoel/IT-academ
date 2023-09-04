3c1ebc537b9628bb1b9c9a72eec34c1d69404f0d
CidavinOdranoel <jolly0872@gmail.com>
2023-09-04 19:11:40 +0300

    solution created

---
+++ AspCourse/AspCourse_Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AspCourse_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
