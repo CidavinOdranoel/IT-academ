1bbaa9c54d71ff92903a9ea3ab1913b2c8ec9723
CidavinOdranoel <jolly0872@gmail.com>
2023-09-14 02:19:24 +0300

    Version 1.0 created
    Base functions, parity check, history.

--- AspCourse/Lesson5/Calculator.cs
+++ AspCourse/Lesson5/Calculator.cs
@@ -8,7 +8,70 @@ namespace Lesson5
 {
     internal class Calculator
     {
-        int _version;
+        // Поля
+        private string _version;
 
+        private double _lastResult;
+
+        private string _history;
+
+        //Свойства
+
+
+        //Конструктор
+        public Calculator()
+        {
+            this._version = "1.0";
+            this._history = "Created\n";
+        }
+
+        public double Sum (double x, double y) 
+        {
+            double result = x + y;
+            this._history += $"Sum: {x} + {y} = {result}\n";
+            this._lastResult = result;
+            return result;
+        }
+
+        public double Difference(double x, double y)
+        {
+            double result = x - y;
+            this._history += $"Difference: {x} - {y} = {result}\n";
+            this._lastResult = result;
+            return result;
+        }
+
+        public double Mult(double x, double y)
+        {
+            double result = x * y;
+            this._history += $"Mult: {x} * {y} = {result}\n";
+            this._lastResult = result;
+            return result;
+        }
+
+        public double Divide(double x, double y)
+        {
+            double result = x / y;
+            this._history += $"Sum: {x} / {y} = {result}\n";
+            this._lastResult = result;
+            return result;
+        }
+
+        public string GetHistory()
+        {
+            return _history;
+        }
+        public string GetVersion()
+        {
+            return _version;
+        }
+        public string GetLastResultString()
+        {
+            return _lastResult.ToString();
+        }
+        public double GetLastResultDouble()
+        {
+            return _lastResult;
+        }
     }
 }
