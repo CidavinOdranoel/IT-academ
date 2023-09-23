d80591d14c23d0e19b386df316e31c62f07dd83f
CidavinOdranoel <jolly0872@gmail.com>
2023-09-23 05:15:35 +0300

    Created

---
+++ AspCourse/Lesson8/Emploee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    internal class Emploee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+}
