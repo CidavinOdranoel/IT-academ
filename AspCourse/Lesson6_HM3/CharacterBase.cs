de2459b82949d4d95e125880be81ee229e0e25da
CidavinOdranoel <jolly0872@gmail.com>
2023-09-23 07:17:47 +0300

    Base types created
    BaseCharacter, Mage, Thief, Warrior, Mobs

---
+++ AspCourse/Lesson6_HM3/CharacterBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_HM3
+{
+    public abstract class CharacterBase
+    {
+        public string Type { get; init; }
+        public string Status { get; set; }
+        public int CurrentX { get; set; }
+        public int CurrentY { get; set; }
+        public int HealthPoints { get; set; }
+        public int Damgae { get; set; }
+        public int Defense { get; set; }
+
+
+        protected abstract void Attack();
+        protected abstract void Defend();
+        protected void Move(string direction) 
+        {
+            switch (direction)
+            {
+                case "left":
+                    Console.WriteLine($"{Type} is moving {direction}");
+                    CurrentX++;
+                     break;
+                case "right":
+                    Console.WriteLine($"{Type} is moving {direction}");
+                    CurrentX--;
+                     break;
+                case "up":
+                    Console.WriteLine($"{Type} is moving {direction}");
+                    CurrentY++;
+                     break;
+                case "down":
+                    Console.WriteLine($"{Type} is moving {direction}");
+                    CurrentY--;
+                     break;
+                default: throw new NotImplementedException("Invalid movement direction");
+            }
+        }
+
+    }
+}
