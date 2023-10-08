fc4a5622a296af60c1121f495efdbccbd45ec18f
CidavinOdranoel <jolly0872@gmail.com>
2023-10-08 19:57:58 +0300

    Created + Task1

---
+++ AspCourse/Lesson10_HM5/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+
+namespace Lesson10_HM5
+{
+    internal class Program
+    {
+        static void PrintArray(ArrayList list)
+        {
+            foreach (var i in list)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+        static void PrintArray(List<int> list)
+        {
+            foreach (var i in list)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            #region TASK 1
+            // TASK 1
+
+            // Создание массивов
+            ArrayList list1 = new ArrayList() {"One", "Two" };
+            List<int> list2 = new List<int>() { 1, 2 };
+            Console.WriteLine("Создание массивов:");
+            Console.Write("ArrayList: ");
+            PrintArray(list1);
+            Console.Write("List<int>: ");
+            PrintArray(list2);
+            Console.WriteLine();
+
+            // Добавление элементов в списки
+            list1.Add("Three"); list1.Add("Four");
+            list2.Add(3); list2.Add(4);
+            Console.WriteLine("Добавление элементов в списки:");
+            Console.Write("ArrayList: ");
+            PrintArray(list1);
+            Console.Write("List<int>: ");
+            PrintArray(list2);
+            Console.WriteLine();
+
+            // Удаление элемента с индексом 3
+            list1.RemoveAt(3);
+            list2.RemoveAt(3);
+            Console.WriteLine("Удаление элемента с индексом 3:");
+            Console.Write("ArrayList: ");
+            PrintArray(list1);
+            Console.Write("List<int>: ");
+            PrintArray(list2);
+            Console.WriteLine();
+
+            // Удение одного добавленного элемента
+            list1.Remove("Three");
+            list2.Remove(3);
+            Console.WriteLine("Удаление одного добавленного элемента:");
+            Console.Write("ArrayList: ");
+            PrintArray(list1);
+            Console.Write("List<int>: ");
+            PrintArray(list2);
+            Console.WriteLine();
+
+            // Сортировка
+            list1.Sort();  
+            list2.Sort();
+            Console.WriteLine("Сортировка:");
+            Console.Write("ArrayList: ");
+            PrintArray(list1);
+            Console.Write("List<int>: ");
+            PrintArray(list2);
+            Console.WriteLine();
+
+            #endregion
+
+            #region TASK 2
+
+            //  Создайте пользовательский список.
+            // Реализуйте добавление в список нового объекта так,
+            // чтобы после добавления список сортировался по одному из свойств вашего класса.
+
+            // Заполните список начальными данными.
+            // Добавьте в ваш список новый элемент.
+
+            // После каждого изменения списка выводите его на экран.
+
+            Task2 aga = new Task2(3, "3");
+
+            #endregion
+
+        }
+    }
+}
---
+++ AspCourse/Lesson10_HM5/Task2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10_HM5
+{
+    internal class Task2<T>
+    {
+        //  Создайте пользовательский список.
+        // Реализуйте добавление в список нового объекта так,
+        // чтобы после добавления список сортировался по одному из свойств вашего класса.
+
+        // Заполните список начальными данными.
+        // Добавьте в ваш список новый элемент.
+
+        // После каждого изменения списка выводите его на экран.
+
+        public T[] Items { get; set; }
+        public string Comment { get; set; }
+
+        public Task2(int number, string text, string comment = "No comment") 
+        {
+
+        }
+    }
+}
