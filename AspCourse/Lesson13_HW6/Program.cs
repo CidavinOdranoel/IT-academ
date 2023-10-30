c75b4c7c0b570b214254c1d0530539cd3bcdd0ee
CidavinOdranoel <jolly0872@gmail.com>
2023-10-31 01:54:05 +0300

    Created

---
+++ AspCourse/Lesson13_HW6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson13_HW6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
