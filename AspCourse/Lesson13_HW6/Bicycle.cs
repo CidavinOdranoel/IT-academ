b24b175a9cf7f579d1693aebddf4b5d0d0fc8389
CidavinOdranoel <jolly0872@gmail.com>
2023-10-31 02:58:09 +0300

    Bicycle done

--- AspCourse/Lesson13_HW6/Bicycle.cs
+++ AspCourse/Lesson13_HW6/Bicycle.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Lesson13_HW6
 {
-    internal class Bicycle
+    internal class Bicycle : Vehicle
     {
+        internal Bicycle(string bicycleType, string color = "blue", int maxSpeed = 50, int baseFuel = 30)
+        {
+            _transportType = "Bicycle";
+            BicycleType = bicycleType;
+            Color = color;
+            MaxSpeed = maxSpeed;
+            Fuel = baseFuel;
+        }
+
+        string BicycleType { get; set; }
+
+        internal new void Move()
+        {
+            Console.WriteLine($"Someone is riding a bicycle. It's {BicycleType}");
+        }
+
+        internal override bool ToFuel(int fuelAmount)
+        {
+            Console.WriteLine("Bicycle can not be fueled");
+            return false;
+        }
     }
 }
