8e4992d100676c93f6845f8a6dddf146f27f0cae
CidavinOdranoel <jolly0872@gmail.com>
2023-10-31 03:13:52 +0300

    Motorbike done

--- AspCourse/Lesson13_HW6/Motorbike.cs
+++ AspCourse/Lesson13_HW6/Motorbike.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Lesson13_HW6
 {
-    internal class Motorbike
+    internal class Motorbike : Vehicle
     {
+        internal Motorbike(int capacity, string color = "red", int maxSpeed = 200, int baseFuel = 30)
+        {
+            _transportType = "Motorbike";
+            EngineCapacity = capacity;
+            Color = color;
+            MaxSpeed = maxSpeed;
+            Fuel = baseFuel;
+        }
+
+        internal int EngineCapacity { get; init; }
+
+        // Move() override is not needed
+
+        internal override bool ToFuel(int fuelAmount)
+        {
+            if (!base.ToFuel(fuelAmount))
+            {               
+                return false;
+            }
+            if (Fuel > EngineCapacity)
+            {
+                Console.WriteLine("Fuel capacity has been exeeded.");
+                Fuel = EngineCapacity;
+                return true;
+            }
+            return true;
+        }
     }
 }
