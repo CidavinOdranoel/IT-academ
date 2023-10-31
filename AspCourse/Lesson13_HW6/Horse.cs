5ea4e944ad92489466ea8b6cab69181c45c00688
CidavinOdranoel <jolly0872@gmail.com>
2023-10-31 03:23:57 +0300

    Horse done

---
+++ AspCourse/Lesson13_HW6/Horse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13_HW6
+{
+    internal class Horse :Vehicle
+    {
+        internal Horse(string name, string breed, string color = "brown", int maxSpeed = 80, int baseEnergy = 30) 
+        {
+            _transportType = "Horse";
+            Name = name;
+            Breed = breed;
+            Color = color;
+            MaxSpeed = maxSpeed;
+            Fuel = baseEnergy;
+        }
+
+        internal string Name { get; set; }
+        internal string Breed { get; set; }
+
+        private const int MaxEnergy = 100;
+
+        internal override void Move()
+        {
+            Console.WriteLine("Horse is running...");
+        }
+
+        /// <summary>
+        /// Refill the energy of thhe horse
+        /// </summary>
+        internal new void ToFuel()
+        {
+            Console.WriteLine("Horse is resting and regaining energy...");
+            Fuel = MaxEnergy;
+        }
+    }
+}
